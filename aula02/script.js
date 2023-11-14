e238a2bea49fcee3ada5de86ab3ce276c17608b7
Helen <fer.helen@icloud.com>
2023-11-14 19:53:03 +0000

    add button

---
+++ aula02/script.js
@@ -0,0 +1,19 @@
+function comecar() {
+    var numeroSecreto = parseInt(Math.random() * 1001)
+
+    while (chute != numeroSecreto){
+    var chute = prompt("Digite um número de 0 a 1000")
+
+    if(chute == numeroSecreto){
+    window.alert("Acertou!")
+}
+    else if(chute > numeroSecreto){
+    window.alert("Errou! O número secreto é menor")
+}
+    else if (chute < numeroSecreto){
+    window.alert("Errou! o número é maior")
+    }
+}
+
+}
+
