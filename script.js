81b33ab76564011f4841d30fed9db1fe5e08ff4d
Helen <fer.helen@icloud.com>
2023-11-14 19:40:30 +0000

    add chalenge files

--- script.js
+++ script.js
@@ -13,11 +13,3 @@ while (chute != numeroSecreto){
         window.alert("Errou! o número é maior")
     }
 }
-
-
-
-// desafio 1 - errou, esse numero é maior/menor do que.../  concatenacao de string, usar outro alert p avisar quantos chutes ja dei var numerodechutes = a cada iteração (laço) while valha ela mesma +1 ou seja ++ incrementar
-
-// numero entre 1 e 1000 de forma que o 0 nao apareça
-
-// qual é o numero maximo de tentativas que eu tenho que ter maximo pra acertar (dividir em 4 )
