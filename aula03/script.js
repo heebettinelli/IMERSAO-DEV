b89e0a2c949f2055a0ba22fff893b898e4e7a1e8
Helen <fer.helen@icloud.com>
2023-11-15 20:13:32 +0000

    chalenge one

---
+++ aula03/script.js
@@ -0,0 +1,7 @@
+var cartazesNetlix = ["https://upload.wikimedia.org/wikipedia/pt/c/c1/Twilight_Poster.jpg", "https://upload.wikimedia.org/wikipedia/pt/1/1d/Harry_Potter_Pedra_Filosofal_2001.jpg", "https://2.bp.blogspot.com/-QudkbDiu5x0/WtoC0oCeNAI/AAAAAAAAFew/CNN7qbpdKE0omBdJz67e53bjddiRaRMRACLcBGAs/s640/my-neighbor-totoro-picture-book-D_NQ_NP_835776-MLB26662493451_012018-F.jpg", "https://m.media-amazon.com/images/M/MV5BNDVkYjU0MzctMWRmZi00NTkxLTgwZWEtOWVhYjZlYjllYmU4XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX1000_.jpg", "https://pics.filmaffinity.com/One_Piece_TV_Series-985097858-large.jpg", "https://br.web.img3.acsta.net/pictures/14/02/04/13/20/128334.jpg"  
+]
+
+
+for (var i = 0; i < cartazesNetlix.length; i++) {
+    document.write("<img src=" + cartazesNetlix[i] + ">")
+}
